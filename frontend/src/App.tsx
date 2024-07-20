9fdb32e13a761b5f072172ed0573267b698ee2c4
Isuru Dinusha <isurudinusha@gmail.com>
2024-07-20 12:19:04 +0530

    chore: Update CSS to add gap between elements in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,21 @@ import SignUp from "./components/sign-up/sign-up.component";
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-      <SignUp />
-    </div>
+    <>
+      <span className="title">
+        Click the link to start API service:{" "}
+        <a
+          href="https://jwt-auth-with-bcrypt-by-isuru.onrender.com/"
+          target="_blank"
+        >
+          Start API
+        </a>
+      </span>
+      <div className="App">
+        <SignIn />
+        <SignUp />
+      </div>
+    </>
   );
 }
 
